694958a7465bc25bf1ee601ed7e78625d1830368
Adithyan M.S <adithyan2ms@gmail.com>
2021-09-14 16:24:40 +0530

    CaptureImage Added

--- app/src/main/java/com/reportit/reportit/MainActivity.kt
+++ app/src/main/java/com/reportit/reportit/MainActivity.kt
@@ -1,12 +1,27 @@
 package com.reportit.reportit
 
+import android.content.Intent
+import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var our_request_code: Int =123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+    fun takePhoto(view: View){
+        val intent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if(intent.resolveActivity(packageManager)!=null){
+            startActivityForResult(intent,our_request_code)
+        }
     }
 }
