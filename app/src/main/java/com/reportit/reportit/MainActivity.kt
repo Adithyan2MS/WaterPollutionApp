60cb4f8cc7cc57f265e470141fa9a359ef6890a4
Adithyan M.S <adithyan2ms@gmail.com>
2021-09-14 20:12:13 +0530

    imageview added

--- app/src/main/java/com/reportit/reportit/MainActivity.kt
+++ app/src/main/java/com/reportit/reportit/MainActivity.kt
@@ -7,10 +7,10 @@ import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.add_details.*
 
 class MainActivity : AppCompatActivity() {
 
-    private var our_request_code: Int =123
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,13 +20,25 @@ class MainActivity : AppCompatActivity() {
             var intent=Intent(Intent.ACTION_GET_CONTENT)
             intent.setType("image/*")
 
-            startActivity(intent)
+            startActivityForResult(intent,456)
         }
     }
     fun takePhoto(view: View){
         val intent= Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         if(intent.resolveActivity(packageManager)!=null){
-            startActivityForResult(intent,our_request_code)
+            startActivityForResult(intent,123)
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        setContentView(R.layout.add_details)
+        if(requestCode==123){
+            var bmp: Bitmap =data?.extras?.get("data") as Bitmap
+            imageView.setImageBitmap(bmp)
+        }else if(requestCode==456){
+            imageView.setImageURI(data?.data)
+        }
+    }
+
 }
