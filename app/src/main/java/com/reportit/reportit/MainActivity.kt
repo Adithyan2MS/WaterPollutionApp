1a9f30e7a84678bc7ecd25e197dd00104b715ff9
Adithyan M.S <adithyan2ms@gmail.com>
2021-09-16 12:41:54 +0530

    back to normal

--- app/src/main/java/com/reportit/reportit/MainActivity.kt
+++ app/src/main/java/com/reportit/reportit/MainActivity.kt
@@ -29,7 +29,6 @@ class MainActivity : AppCompatActivity() {
 
         }
     }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         setContentView(R.layout.add_details)
