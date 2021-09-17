31cc244c85cd412be23fcdfbffa763276d3c96ed
Adithyan M.S <adithyan2ms@gmail.com>
2021-09-17 21:27:51 +0530

    some issues

--- app/src/main/java/com/reportit/reportit/MainActivity.kt
+++ app/src/main/java/com/reportit/reportit/MainActivity.kt
@@ -2,12 +2,12 @@ package com.reportit.reportit
 
 import android.content.Intent
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.add_details.*
 import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity() {
@@ -21,27 +21,33 @@ class MainActivity : AppCompatActivity() {
     fun takePhoto(view: View) {
         var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         if (intent.resolveActivity(packageManager) != null) {
-            startActivityForResult(intent, 123)
+            startActivityForResult(intent,123)
         }
     }
 
     fun pickPhoto(view: View) {
-        val intent=Intent(Intent.ACTION_PICK)
+        var intent=Intent(Intent.ACTION_PICK)
         intent.type="image/*"
         startActivityForResult(intent,456)
     }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         val intent=Intent(this,PreviewActivity::class.java)
-        if(requestCode==123){
-            var bmp: Bitmap =data?.extras?.get("data") as Bitmap
-            var bstream=ByteArrayOutputStream()
-            bmp.compress(Bitmap.CompressFormat.PNG,50,bstream)
-            val byteArray=bstream.toByteArray()
-            intent.putExtra("image",byteArray)
-        }
+        intent.putExtra("reqcode",requestCode)
+
+        var bmp: Bitmap =data?.extras?.get("data") as Bitmap
+        var bstream=ByteArrayOutputStream()
+        bmp.compress(Bitmap.CompressFormat.PNG,50,bstream)
+        val byteArray=bstream.toByteArray()
+        intent.putExtra("image",byteArray)
+
+        var uri = data!!.data
+        intent.putExtra("imagePath", uri.toString())
+
         startActivity(intent)
-        //intent.putExtra("reqcode",requestCode)
+
+
+        //
         //intent.putExtra("rescode",resultCode)
 
         //setContentView(R.layout.add_details)
--- app/src/main/java/com/reportit/reportit/PreviewActivity.kt
+++ app/src/main/java/com/reportit/reportit/PreviewActivity.kt
@@ -3,6 +3,7 @@ package com.reportit.reportit
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_preview.*
@@ -12,14 +13,24 @@ class PreviewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
-        val intent=getIntent();
-        var bitmap : Bitmap? =null
-        if (intent.hasExtra("image")){
-            //convert to bitmap
-            val byteArray = intent.getByteArrayExtra("image")
-            bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
-            imageView.setImageBitmap(bitmap)
+        var intent=getIntent();
 
+        var requestCode=intent.getIntExtra("reqcode",0)
+
+        if(requestCode==123){
+            var bitmap : Bitmap? =null
+            if (intent.hasExtra("image")){
+                //convert to bitmap
+                val byteArray = intent.getByteArrayExtra("image")
+                bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+                imageView.setImageBitmap(bitmap)
+            }
+        }
+        else if(requestCode==456){
+            var image_path= intent.getStringExtra("imagePath");
+
+            var fileUri = Uri.parse(image_path)
+            imageView.setImageURI(fileUri)
         }
 
 
