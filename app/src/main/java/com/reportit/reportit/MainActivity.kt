2b903ccc4b26ba8f4b323aee4aee5b94df13be9c
Adithyan M.S <adithyan2ms@gmail.com>
2021-09-18 21:39:07 +0530

    image Picker and Capture passed to new activity(Done)

--- app/src/main/java/com/reportit/reportit/MainActivity.kt
+++ app/src/main/java/com/reportit/reportit/MainActivity.kt
@@ -10,7 +10,6 @@ import java.io.ByteArrayOutputStream
 
 class MainActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/reportit/reportit/PreviewActivity.kt
+++ app/src/main/java/com/reportit/reportit/PreviewActivity.kt
@@ -1,14 +1,10 @@
 package com.reportit.reportit
 
-import android.graphics.Bitmap
 import android.graphics.BitmapFactory
-import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_preview.*
-import java.io.FileInputStream
-import java.lang.Exception
 
 class PreviewActivity : AppCompatActivity() {
 
@@ -19,21 +15,14 @@ class PreviewActivity : AppCompatActivity() {
         var intent=getIntent();
 
         if (intent.hasExtra("image")){
-            //convert to bitmap
             val byteArray = intent.getByteArrayExtra("image")
             val bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
-
             imageView.setImageBitmap(bmp)
-
         }
-
-
-        var image_path= intent.getStringExtra("imagePath");
-        var fileUri = Uri.parse(image_path)
-        if(fileUri!=null){
+        else if(intent.hasExtra("imagePath")){
+            var image_path= intent.getStringExtra("imagePath");
+            var fileUri = Uri.parse(image_path)
             imageView.setImageURI(fileUri)
-
         }
-
     }
 }
