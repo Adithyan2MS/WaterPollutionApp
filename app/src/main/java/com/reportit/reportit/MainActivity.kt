e0406c98dc33e3e61a22554b187f99a7f4f74ffe
Adithyan M.S <adithyan2ms@gmail.com>
2021-09-18 20:51:03 +0530

    nullable reciever error

--- app/src/main/java/com/reportit/reportit/MainActivity.kt
+++ app/src/main/java/com/reportit/reportit/MainActivity.kt
@@ -2,11 +2,9 @@ package com.reportit.reportit
 
 import android.content.Intent
 import android.graphics.Bitmap
-import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
-import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.ByteArrayOutputStream
 
@@ -18,46 +16,33 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         imageButton4.setOnClickListener {
-            var intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            if (intent.resolveActivity(packageManager) != null) {
-                startActivityForResult(intent, 123)
+            var imgCap = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            if (imgCap.resolveActivity(packageManager) != null) {
+                startActivityForResult(imgCap,123)
             }
         }
         imageButton5.setOnClickListener{
-            val intent=Intent(Intent.ACTION_PICK)
-            intent.type="image/*"
-            startActivityForResult(intent,456)
-
+            var imgPick=Intent(Intent.ACTION_PICK)
+            imgPick.type="image/*"
+            startActivityForResult(imgPick,456)
         }
     }
-
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
         val intent=Intent(this,PreviewActivity::class.java)
-        intent.putExtra("reqcode",requestCode)
 
-        var bmp: Bitmap =data?.extras?.get("data") as Bitmap
-        var bstream=ByteArrayOutputStream()
-        bmp.compress(Bitmap.CompressFormat.PNG,50,bstream)
-        val byteArray=bstream.toByteArray()
-        intent.putExtra("image",byteArray)
-
-        val uri = data.data
-        intent.putExtra("imagePath", uri.toString())
-
+        if(requestCode==123){
+            var bmp: Bitmap =data?.extras?.get("data") as Bitmap
+            val stream = ByteArrayOutputStream()
+            bmp.compress(Bitmap.CompressFormat.PNG, 100, stream)
+            val byteArray = stream.toByteArray()
+            intent.putExtra("image", byteArray)
+        }
+        else if(requestCode==456){
+            var Uri = data!!.data
+            intent.putExtra("imagePath", Uri.toString())
+        }
         startActivity(intent)
-
-
-        //
-        //intent.putExtra("rescode",resultCode)
-
-        //setContentView(R.layout.add_details)
-//        if(requestCode==123){
-//            var bmp: Bitmap =data?.extras?.get("data") as Bitmap
-//            imageView.setImageBitmap(bmp)
-//        }else if(requestCode==456){
-//            imageView.setImageURI(data?.data)
-//        }
-
     }
 }
--- app/src/main/java/com/reportit/reportit/PreviewActivity.kt
+++ app/src/main/java/com/reportit/reportit/PreviewActivity.kt
@@ -7,31 +7,33 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_preview.*
+import java.io.FileInputStream
+import java.lang.Exception
 
 class PreviewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_preview)
+
         var intent=getIntent();
 
-        var requestCode=intent.getIntExtra("reqcode",0)
+        if (intent.hasExtra("image")){
+            //convert to bitmap
+            val byteArray = intent.getByteArrayExtra("image")
+            val bmp = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
 
-        if(requestCode==123){
-            var bitmap : Bitmap? =null
-            if (intent.hasExtra("image")){
-                //convert to bitmap
-                val byteArray = intent.getByteArrayExtra("image")
-                bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
-                imageView.setImageBitmap(bitmap)
-            }
+            imageView.setImageBitmap(bmp)
+
         }
-        else if(requestCode==456){
-            var image_path= intent.getStringExtra("imagePath");
-            var fileUri = Uri.parse(image_path)
+
+
+        var image_path= intent.getStringExtra("imagePath");
+        var fileUri = Uri.parse(image_path)
+        if(fileUri!=null){
             imageView.setImageURI(fileUri)
-        }
 
+        }
 
     }
 }
