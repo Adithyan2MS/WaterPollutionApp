c1be0a2b60a39c9f691c3fe88d07344d6de578f9
Adithyan M.S <adithyan2ms@gmail.com>
2022-01-21 12:36:36 +0530

    retrofit version changed(ison)

--- app/src/main/java/com/reportit/reportit/ServiceBuilder.kt
+++ app/src/main/java/com/reportit/reportit/ServiceBuilder.kt
@@ -7,9 +7,9 @@ import retrofit2.converter.gson.GsonConverterFactory
 object ServiceBuilder {
     private val client = OkHttpClient.Builder().build()
 
-    val instance:RestApi by lazy{
-         val retrofit = Retrofit.Builder()
-            .baseUrl("http://192.168.56.1:2001") // change this IP for testing by your actual machine IP
+    val instance: RestApi by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://10.0.2.2:2001") // change this IP for testing by your actual machine IP
             .addConverterFactory(GsonConverterFactory.create())
             .client(client)
             .build()
