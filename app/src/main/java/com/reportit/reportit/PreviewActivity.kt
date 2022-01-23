47257e4158802950bf8a3ee1453d59e9e7f53a24
Adithyan M.S <adithyan2ms@gmail.com>
2022-01-23 17:29:07 +0530

    LOCATION AND DESCRIPTION STORED TO DB

--- app/src/main/java/com/reportit/reportit/PreviewActivity.kt
+++ app/src/main/java/com/reportit/reportit/PreviewActivity.kt
@@ -1,36 +1,19 @@
 package com.reportit.reportit
 
-import com.reportit.reportit.Retrofit.MyService
-import com.reportit.reportit.Retrofit.RetrofitClient
+
 import android.graphics.BitmapFactory
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD
-import android.text.TextUtils
+
 import android.widget.Toast
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
-=======
-import android.view.View
-import android.widget.Toast
->>>>>>> 15a718e
+
 import kotlinx.android.synthetic.main.activity_preview.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class PreviewActivity : AppCompatActivity() {
-
-    lateinit var myService:MyService
-    internal var compositeDisposable:CompositeDisposable= CompositeDisposable()
-
-    override fun onStop(){
-        compositeDisposable.clear()
-        super.onStop()
-    }
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -47,37 +30,7 @@ class PreviewActivity : AppCompatActivity() {
             val fileUri = Uri.parse(imgpath)
             imageView.setImageURI(fileUri)
         }
-<<<<<<< HEAD
-        val retrofit=RetrofitClient.getInstance()
-        myService=retrofit.create(MyService::class.java)
 
-        nextButton.setOnClickListener {
-            storedata(locationEdit.text.toString(),descriptionEdit.text.toString())
-        }
-    }
-
-    private fun storedata(location: String, description: String) {
-
-        if(TextUtils.isEmpty(location))
-        {
-            Toast.makeText ( this@PreviewActivity, "location cannot be null or empty", Toast.LENGTH_SHORT).show()
-            return
-        }
-
-        if(TextUtils.isEmpty(description))
-        {
-            Toast.makeText ( this@PreviewActivity, "description cannot be null or empty", Toast.LENGTH_SHORT).show()
-            return
-        }
-        compositeDisposable.add(myService.storedata(location,description)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe { result ->
-                Toast.makeText(this@PreviewActivity, "" + result, Toast.LENGTH_SHORT).show()
-
-            }
-        )
-=======
         next_btn.setOnClickListener{
             val location=locationEdit.text.toString().trim()
             val description=descriptionEdit.text.toString().trim()
@@ -109,7 +62,6 @@ class PreviewActivity : AppCompatActivity() {
 
         }
 
->>>>>>> 15a718e
     }
 
 }
